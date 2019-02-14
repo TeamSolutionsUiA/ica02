80407e0e699d1463721a0bc0f7b8a749fbd6bc63
Rexsect <46561873+Rexsect@users.noreply.github.com>
2019-02-14 13:53:17 +0100

    test

--- is105-ica02-master/ascii/ascii/ascii.go
+++ is105-ica02-master/ascii/ascii/ascii.go
@@ -22,7 +22,7 @@ const ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
 // 3F ? 111111
 // 40 @ 1000000
 // ...
-func GetASCIIStringLitral()string {
+func GetASCIIStringLitral() {
 	// Kode for Oppgave 1a
 
 	for i := 0; i < len(ascii); i++ {
--- is105-ica02-master/ascii/main_ascii.go
+++ is105-ica02-master/ascii/main_ascii.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	ascii.IterateOverASCIIStringLiteral()
+	ascii.GetASCIIStringLitral()
 }
