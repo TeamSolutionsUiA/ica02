d92383e42c05ba08e9390f5f63ca124db4311e4f
Jonathan <jonatl14@uia.no>
2019-03-01 12:27:14 +0100

    unicode

--- is105-ica02-master/unicode/main_unicode.go
+++ is105-ica02-master/unicode/main_unicode.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	unicode "github.com/TeamSolutionsUiA/ica02/is105-ica02-master/unicode/uniPack"
 )
 
 func main() {
 
-	arguments := os.Args
+	// Test av funksjonen Translate for Islandsk og Japansk oversettelse.
+	input := "\xE2\x80\x9C\x6E\x6F\x72\x64\x20\x6F\x67\x20\x73\xC3\xB8\x72\xE2\x80\x9D\xE2\x80\x8B"
+	outputIs := unicode.Translate(input, "is")
+	outputJp := unicode.Translate(input, "jp")
 
-	output := unicode.Translate(arguments[1], arguments[2])
-	fmt.Println(output)
+	fmt.Println(outputIs)
+	fmt.Println(outputJp)
 
+	//Test av UnicodeCodePointDemo
+	unicode.UnicodeCodePointDemo()
 }
