708c992602118318ed3f37da6104c93b1b87f59f
Jonathan <jonatl14@uia.no>
2019-03-01 12:56:47 +0100

    siste unicode versj

--- is105-ica02-master/unicode/uniPack/unicode.go
+++ is105-ica02-master/unicode/uniPack/unicode.go
@@ -6,31 +6,26 @@ import (
 
 // Kode for Oppgave 4a
 func Translate(expression string, language string) string {
-	transJp := "\xe2\x80\x9c\xe5\x8c\x97\xe3\x81\xa8\xe5\x8d\x97\xe2\x80\x9d"
-	transIs := "\x20\x1C\x6E\x6F\x72\xF0\x75\x72\x20\x6F\x67\x20\x73\x75\xF0\x75\x72\x20\x1D\x0A"
+	transJp := "\x20\x70\xC3\xA5\x20\x6A\x61\x70\x61\x6E\x73\x6B\x20\x65\x72\x20\xE2\x80\x8B" +
+		"\xE2\x80\x9E\xE5\x8C\x97\xE3\x81\xA8\xE5\x8D\x97\xE2\x80\x9D"
+	transIs := "\x20\x70\xC3\xA5\x20\x69\x73\x6C\x61\x6E\x64\x73\x6B\x20\x65\x72\x20\xE2\x80\x8B" +
+		"\xE2\x80\x9C\x6E\x6F\x72\xC3\xB0\x75\x72\x20\x6F\x67\x20\x73\x75\xC3\xB0\x75\x72\xE2\x80\x9D"
 
-	returnString := ""
-	testString := ""
-	if expression == "nord og sor" {
+	returnString := expression
 
-		if language == "jp" {
-			returnString = transJp
-		} else if language == "is" {
-			returnString = transIs
+	if language == "jp" {
+		returnString += transJp
+	} else if language == "is" {
+		returnString += transIs
 
-		}
-		for teller := 0; teller < len(returnString); teller++ {
-			testString += fmt.Sprintf("%c", returnString[teller])
-		}
-
-	} else {
-		testString = "invalid expression! "
 	}
-	return testString
+	returnString = fmt.Sprintf("%s", returnString)
+	return returnString
 }
 
 // Kode for Oppgave 4b
 func UnicodeCodePointDemo() {
+
 	// Hva er dette?
 	// Er det likt på MS Windows og macOS?
 	fmt.Println("\xf0\x9F\x98\x80")
