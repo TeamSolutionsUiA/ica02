e19878173f54cef08b863e2876328478190c4889
Are <arefjerm@gmail.com>
2019-03-04 13:13:55 +0100

    3c

--- is105-ica02-master/treasure/treasure.go
+++ is105-ica02-master/treasure/treasure.go
@@ -1,11 +1,17 @@
 package treasure
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 // PrintTreasureUTF8 tar en "string literal" som INN-data
 // og skriver ut en korrekt text på med UTF-8 koding
 // Koden er for Oppgave 3c
 // Bruk strengen fra filen treasure.txt som in-data for denne funksjonen
 func PrintTreasureUTF8(treasureString string) {
-	fmt.Printf("%s \n%c", treasureString, treasureString)
+	treasureByte := []byte(treasureString)
+	treasureByte = bytes.Replace([]byte(treasureByte), []byte("\xf8"), []byte("\xc3\xb8"), 1)
+	treasureByte = bytes.Replace([]byte(treasureByte), []byte("\xe6"), []byte("\xc3\xa6"), 1)
+	fmt.Printf("%s", treasureByte)
 }
