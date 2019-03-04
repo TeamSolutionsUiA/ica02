26a44405943589daf6aa3fe0d44c9c17bdc6dab5
Are <arefjerm@gmail.com>
2019-03-04 12:12:20 +0100

    fjerna mappe

--- is105-ica02-master/treasure/treasure.go
+++ is105-ica02-master/treasure/treasure.go
@@ -1,9 +1,11 @@
 package treasure
 
+import "fmt"
+
 // PrintTreasureUTF8 tar en "string literal" som INN-data
 // og skriver ut en korrekt text på med UTF-8 koding
 // Koden er for Oppgave 3c
 // Bruk strengen fra filen treasure.txt som in-data for denne funksjonen
 func PrintTreasureUTF8(treasureString string) {
-	
+	fmt.Printf("%s \n%c", treasureString, treasureString)
 }
