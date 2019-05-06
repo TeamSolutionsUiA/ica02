05a6cf8866d8528625de843cc747ca187484928b
Are <arefjerm@gmail.com>
2019-05-06 11:15:34 +0200

    fiksa import

--- is105-ica02-master/fileutils/main_fileutils.go
+++ is105-ica02-master/fileutils/main_fileutils.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	lang01 := fileutils.FileToByteslice("C:/Users/mjubo/Go/src/github.com/TeamSolutionsUiA/ica02/is105-ica02-master/files/lang01.wl")
-	lang02 := fileutils.FileToByteslice("C:/Users/mjubo/Go/src/github.com/TeamSolutionsUiA/ica02/is105-ica02-master/files/lang02.wl")
-	lang03 := fileutils.FileToByteslice("C:/Users/mjubo/Go/src/github.com/TeamSolutionsUiA/ica02/is105-ica02-master/files/lang03.wl")
+	lang01 := fileutils.FileToByteslice("../files/lang01.wl")
+	lang02 := fileutils.FileToByteslice("../files/lang02.wl")
+	lang03 := fileutils.FileToByteslice("../files/lang03.wl")
 	fmt.Printf("%c \n%c \n%c", lang01, lang02, lang03)
 }
